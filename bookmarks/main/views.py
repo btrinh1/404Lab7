9b37ae96115f2cb767b537c17a44005c7da42101
btrinh1 <btrinh@ualberta.ca>
2015-02-25 23:06:24 -0700

    mostly working

--- bookmarks/main/views.py
+++ bookmarks/main/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.shortcuts import redirect
 
-# Create your views here.
+from main.models import Link
+from main.models import Tag
+
+def index(request):
+    context = RequestContext(request)
+    links = Link.objects.all()
+    return render_to_response('main/index.html',{'links': links}, context)
+
+def tags(request):
+    context = RequestContext(request)
+    tags = Tag.objects.all()
+    return render_to_response('main/tags.html',{'tags':tags}, context)
+
+def tag(request, tag_name):
+    context = RequestContext(request)
+    the_tag = Tag.objects.get(name=tag_name)
+    links= the_tag.link_set.all()
+    return render_to_response('main/index.html', {'links': links, 'tag_name':'#' +tag_name}, context)
+
+def add_link(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        url = request.POST.get("url", "")
+        tags = request.POST.get("tags","")
+        title = request.POST.get("title","")
+        splittag = tags.split()
+    tags = tags.split(',')
+    myLink = Link.objects.get_or_create(title=title, url=url)[0]
+    for n in tags:
+    	myLink.tags.add(Tag.objects.get_or_create(name=n)[0])
+    
+    
+    return redirect(index)
